a81c618c2500039bd8781a6ecf44c7bfc3addf91
Victor <em3888@gmail.com>
2015-05-19 18:40:21 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a= 10;
+
+        int b= 20;
+
+        System.out.println("Something really wrong"+ a+ b);
+
+    }
+}
